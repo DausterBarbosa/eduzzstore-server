6e105709db5058108d2cfbaf91f5e92138e32666
dausterbarbosa <dausterbarbosa12@gmail.com>
2021-11-26 11:28:42 -0300

    enviroments variables confiigured

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ require('dotenv/config');
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@eduzzstore.wqllw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `${process.env.DB_URL}`
 ).then((response) => {
     console.log(response);
 }).catch((err) =>{
